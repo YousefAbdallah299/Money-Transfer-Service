52c6a0a14cdf149f18c875335baecf7141ea07de
Ahmed Amin <ahmedamin372002@gmail.com>
2024-09-08 12:28:16 +0300

    Update AuthControllerTest.java

--- src/test/java/com/transfer/controller/AuthControllerTest.java
+++ src/test/java/com/transfer/controller/AuthControllerTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthControllerTest {
+    //testing auth controller
 
     @Autowired
     private MockMvc mockMvc;
